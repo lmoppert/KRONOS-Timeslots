4a9a0213ef16332c95df76145190ee6a2f90113c
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-22 12:24:10 +0200

    still some bugs in the datepicker styling

--- timeslots/urls.py
+++ timeslots/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from timeslots.models import Station
 
-urlpatterns = patterns('',
+urlpatterns = patterns('timeslots.views',
     url(r'^$', 'index'),
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Station)),
 )
