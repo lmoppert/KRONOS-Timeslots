c0074c76a9d8873c6c3661b2eb4fd527220a0e25
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-23 17:32:06 +0200

    navbar pinping

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'timeslots',
-    'bootstrap_toolkit'
+    'bootstrap_toolkit',
+    'crispy_forms',
 )
 
 # A sample logging configuration. The only tangible logging
--- timeslots/views.py
+++ timeslots/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+#from django.http import HttpResponseRedirect, HttpResponse
+#from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from timeslots.models import Station, Dock, Line
 
 def index(request):
-    stations = get_list_or_404(Station)
-    return render_to_response('index.html', {'station_list': stations}, context_instance=RequestContext(request))
+    station_list = get_list_or_404(Station)
+    return render_to_response('index.html', {'station_list': station_list}, context_instance=RequestContext(request))
