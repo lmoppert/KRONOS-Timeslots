77a7d66b2ae5d892adf27036323877a14b851006
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-24 15:06:31 +0200

    Detail View for station

--- timeslots/urls.py
+++ timeslots/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
-from timeslots.models import Station
+from timeslots.models import Station, Dock, Slot
 
 urlpatterns = patterns('timeslots.views',
     url(r'^$', 'index'),
-    url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Station)),
+    url(r'^station/(?P<pk>\d+)/$', DetailView.as_view(model=Station)),
+    url(r'^dock/(?P<pk>\d+)/$', DetailView.as_view(model=Dock)),
 )
