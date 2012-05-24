ce69762ad7afb7e7bd034e507df3b94d0af38a82
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-24 17:30:04 +0200

    User profile page added

--- timeslots/urls.py
+++ timeslots/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
-from timeslots.models import Station, Dock, Slot
+from timeslots.models import Station, Dock, Slot, UserProfile
 
 urlpatterns = patterns('timeslots.views',
     url(r'^$', 'index'),
     url(r'^station/(?P<pk>\d+)/$', DetailView.as_view(model=Station)),
     url(r'^dock/(?P<pk>\d+)/$', DetailView.as_view(model=Dock)),
+    url(r'^user/(?P<pk>\d+)/$', DetailView.as_view(model=UserProfile)),
 )
