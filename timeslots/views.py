b2cbb17012b2d9671c51faed0a3b8c1f10b50ec6
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-24 17:03:08 +0200

    Show user in navbar

--- timeslots/views.py
+++ timeslots/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response
+from django.contrib.auth.decorators import login_required
 #from django.http import HttpResponseRedirect, HttpResponse
 #from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from timeslots.models import Station, Dock, Line
 
+@login_required
 def index(request):
     station_list = get_list_or_404(Station)
     return render_to_response('index.html', {'station_list': station_list}, context_instance=RequestContext(request))
