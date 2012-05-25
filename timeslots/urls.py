fd4a73dff31bdb67739f2953b009643da72d9669
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-25 15:08:00 +0200

    Station picker functionality added

--- timeslots/urls.py
+++ timeslots/urls.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from timeslots.models import Station, Dock, Slot, UserProfile
@@ -5,7 +6,9 @@ from timeslots.models import Station, Dock, Slot, UserProfile
 urlpatterns = patterns('timeslots.views',
     url(r'^$', 'index'),
     url(r'^logout$', 'logout_then_login'),
-    url(r'^station/(?P<pk>\d+)/$', DetailView.as_view(model=Station)),
+    url(r'^station/(?P<pk>\d+)/date/(?P<date>\d{4}-\d{2}-\d{2})/$', 'station'),
+    url(r'^station/(?P<pk>\d+)/$', 'station', {'date': datetime.now().strftime("%Y-%m-%d")}),
+    url(r'^station/$', 'station_redirect'),
     url(r'^dock/(?P<pk>\d+)/$', DetailView.as_view(model=Dock)),
     url(r'^user/(?P<pk>\d+)/$', DetailView.as_view(model=UserProfile)),
 )
--- timeslots/views.py
+++ timeslots/views.py
@@ -14,3 +14,13 @@ def logout_page(request):
 def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
 
+def station_redirect(request):
+    if request.method == 'POST':
+        station = request.POST['selectedStation']
+        return HttpResponseRedirect('/timeslots/station/%s/' % station)
+
+
+
+def station(request, pk, date):
+    station = get_object_or_404(Station, pk=pk)
+    return render_to_response('timeslots/station_dayview.html', { 'station': station, 'date': date}, context_instance=RequestContext(request))
