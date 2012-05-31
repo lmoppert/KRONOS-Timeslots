f0f4e489d5bcab619cf363f2aca803dfdf44aa04
Lutz Moppert <lutz.moppert@kronosww.com>
2012-05-31 14:01:50 +0200

    created view for a siingle slot

--- timeslots/views.py
+++ timeslots/views.py
@@ -22,4 +22,15 @@ def station_redirect(request):
 def station(request, pk, date):
     station = get_object_or_404(Station, pk=pk)
     return render_to_response('timeslots/station_detail.html', 
-                { 'station': station, 'date': date}, context_instance=RequestContext(request))
+            { 'station': station, 'date': date}, context_instance=RequestContext(request))
+
+def slot(request, date, block_id, index, line):
+    block = get_object_or_404(Block, pk=block_id)
+    try:
+        end = block.start_times[int(index)]
+    except IndexError:
+        end = block.end
+    timeslot = block.start_times[int(index)-1].strftime("%H:%M") + " - " + end.strftime("%H:%M")
+    return render_to_response('timeslots/slot_detail.html', 
+            { 'date': date, 'curr_block': block, 'index':index, 'line':line, 'timeslot':timeslot, 'station':block.dock.station}, 
+            context_instance=RequestContext(request))
