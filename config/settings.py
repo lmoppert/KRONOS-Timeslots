3cf7bdf9594733da3b5829b56519f0229d161939
Lutz Moppert <lutz.moppert@kronosww.com>
2012-06-26 12:07:03 +0200

    fixed fixtures

--- config/settings.py
+++ config/settings.py
@@ -65,8 +65,8 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-#STATIC_URL = 'http://lev-srv-565/static/'
-STATIC_URL = 'http://baerlauch/static/'
+STATIC_URL = 'http://lev-srv-565/static/'
+#STATIC_URL = 'http://baerlauch/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
