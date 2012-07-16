bc2bb8b2baaccc36dc5510754a00731783e0cd9f
Lutz Moppert <lutz.moppert@kronosww.com>
2012-07-16 14:29:12 +0200

    minor fixes

--- timeslots/urls.py
+++ timeslots/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView, ListView
 from timeslots.models import Station, Dock, Slot, UserProfile
 
 urlpatterns = patterns('timeslots.views',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name='timeslots_home'),
     url(r'^logout$', 'logout_then_login', name='user_logout'),
     url(r'^keco/$', 'keco'),
     url(r'^station/(?P<station_id>\d+)/date/(?P<date>\d{4}-\d{2}-\d{2})/$', 'station'),
@@ -13,7 +13,7 @@ urlpatterns = patterns('timeslots.views',
     url(r'^jobs/(?P<station_id>\d+)/date/(?P<date>\d{4}-\d{2}-\d{2})/$', 'jobs'),
     url(r'^date/(?P<date>\d{4}-\d{2}-\d{2})/slot/(?P<block_id>\d+)\.(?P<timeslot>\d+)\.(?P<line>\d+)/$', 'slot', name='timeslots_slot_detail'),
     url(r'^dock/(?P<pk>\d+)/$', DetailView.as_view(model=Dock)),
-    url(r'^profile/(?P<pk>\d+)/$', DetailView.as_view(model=UserProfile, template_name='timeslots/user_detail.html'), name='timeslots_profile_detail'),
+   url(r'^profile/(?P<pk>\d+)/$', DetailView.as_view(model=UserProfile, template_name='timeslots/user_detail.html'), name='timeslots_profile_detail'),
 )
 urlpatterns += patterns('django.contrib.auth.views',
     url(r'^login/$', 'login', {'template_name': 'timeslots/user_login.html'}, name='user_login'),
