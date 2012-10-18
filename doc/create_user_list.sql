2fd0585824dcf8de7fe3d7f9cd87c90db6a89ea9
Lutz Moppert <lutz.moppert@kronosww.com>
2012-10-18 16:44:55 +0200

    production environment cleaned up

--- doc/create_user_list.sql
+++
@@ -1,9 +0,0 @@
-# vim:syn=mysql
-DROP TABLE user_list;
-CREATE TABLE user_list AS (
-    SELECT u.username, u.first_name, u.last_name, u.email, u.is_staff, u.is_active, p.* 
-    FROM `auth_user` u 
-    JOIN `timeslots_userprofile` p
-    WHERE u.id = p.user_id 
-);
-
