33417f1d0497d067a3811a71b41e01e52bc67145
Lutz Moppert <lutz.moppert@kronosww.com>
2012-10-18 14:27:22 +0200

    added local settings option

--- config/settings.py
+++ config/settings.py
@@ -8,97 +8,49 @@ try:
     DJANGO_SERVER = os.environ['DJANGO_SERVER']
 except KeyError:
     DJANGO_SERVER = 'Production'
-
 if DJANGO_SERVER == 'Development':
     DEBUG = True
 else:
     DEBUG = False
-
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     ('Lutz Moppert', 'lutz.moppert@kronosww.com'),
 )
-
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'bX2p?vyXnZwK',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DIR, 'sqlite3.db'),
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'Europe/Berlin'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
-
 ugettext = lambda s: s
-
 LANGUAGES = (
     ('en', ugettext('English')),
     ('de', ugettext('German')),
 )
-
 LOCALE_PATHS = (
     os.path.join(DIR, '..', 'locale'),
 )
 
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-
 STATIC_URL = 'http://' + socket.getfqdn() + '/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
-
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -134,12 +86,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-if DEBUG:
-    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
 ROOT_URLCONF = 'config.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'config.wsgi.application'
 
 TEMPLATE_DIRS = (
@@ -156,18 +104,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'timeslots',
-    #'bootstrap_toolkit',
     'crispy_forms',
     'django_tables2',
-    'south',
 )
-if DEBUG:
-    INSTALLED_APPS += ('debug_toolbar',)
-
-    # Settings for the debug toolbar
-    INTERNAL_IPS = ['127.0.0.1']
-    ALWAYS_SHOW_DEBUG_TOOLBAR = True
-    DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS': False}
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -201,6 +140,20 @@ LOGGING = {
 # Login URL
 LOGIN_URL = "/timeslots/login"
 LOGOUT_URL = "/timeslots/logout"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/timeslots/"
 AUTH_PROFILE_MODULE = 'timeslots.UserProfile'
 SESSION_COOKIE_AGE = 86400
+
+# Optional apps used only for development
+if DJANGO_SERVER == 'Development':
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INSTALLED_APPS += ('debug_toolbar','south')
+    INTERNAL_IPS = ['127.0.0.1']
+    ALWAYS_SHOW_DEBUG_TOOLBAR = True
+    DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS': False}
+
+# Look for a local settings file
+try:
+        from local_settings import *
+except:
+        pass
